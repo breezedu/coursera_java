f2b1bbd56821c58d5951b5afb252a070ffc67363
breezedu <breeze.du@gmail.com>
2015-10-20 23:31:33 -0400

    update sayHello object

--- Class_01_hw.java
+++ Class_01_hw.java
@@ -1,21 +1,46 @@
 package coursera_java_duke;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+//import edu.duke.*;
+
 public class Class_01_hw {
 
 	/***
 	 * What this tiny object hw do
 	 * to printout "Hello Java Class!"
+	 * @throws IOException 
 	 */
-	public void sayHello(){
+	public void sayHello() throws IOException{
 		System.out.println("Hello Java Class!");
+		
+		URL url = new URL("http://www.dukelearntoprogram.com/java/hello_unicode.txt");
+		Scanner scanUrl = new Scanner(url.openStream());
+		
+		while(scanUrl.hasNext()){
+			String line = scanUrl.nextLine();
+			System.out.println(line);
+		}
+		
+		scanUrl.close();
+		//URLResource hello = new URLResource("http://www.dukelearntoprogram.com/java/hello_unicode.txt");
+		/***
+		 for(String line : hello.lines(){
+		 	System.out.println(line);
+		 	}
+		 * 
+		 */
 	} //end of sayHello object
 	
 	
 	/****
 	 * the main() function
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		//create a new object Class_01_hw, name hw1
 		Class_01_hw hw1 = new Class_01_hw();
 		
