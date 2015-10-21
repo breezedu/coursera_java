5bbd3f43b24283fbbe046dd8b2f135a38a17d1ed
breezedu <breeze.du@gmail.com>
2015-10-20 23:13:57 -0400

    update Class_01_hw class

--- Class_01_hw.java
+++ Class_01_hw.java
@@ -4,17 +4,23 @@ public class Class_01_hw {
 
 	/***
 	 * What this tiny object hw do
+	 * to printout "Hello Java Class!"
 	 */
-	public void run(){
+	public void sayHello(){
 		System.out.println("Hello Java Class!");
-	} 
+	} //end of sayHello object
+	
 	
 	/****
 	 * the main() function
 	 * @param args
 	 */
 	public static void main(String[] args){
+		//create a new object Class_01_hw, name hw1
 		Class_01_hw hw1 = new Class_01_hw();
-		hw1.run();
-	}
-}
+		
+		//call one method in hw1, sayHello to printout "Hello Java Class!".
+		hw1.sayHello();
+	}//end of main()
+	
+}//ee
