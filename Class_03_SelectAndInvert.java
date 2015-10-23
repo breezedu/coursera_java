f2c36e8ea372ad2c0806b9fdd774cfa5a47e10e5
breezedu <breeze.du@gmail.com>
2015-10-23 01:36:12 -0400

    update SelectAndInvert.java class

--- Class_03_SelectAndInvert.java
+++ Class_03_SelectAndInvert.java
@@ -5,24 +5,47 @@ import java.io.File;
 import edu.duke.DirectoryResource;
 import edu.duke.ImageResource;
 
+/**************
+ * Select some images from one folder
+ * then transfer these images into inverted color
+ * save them to the project folder, with a "inver-" in front of the original names
+ * 
+ * @author Jeff
+ *
+ */
 public class Class_03_SelectAndInvert {
 	
+	/*******
+	 * SelectAndInvert() method
+	 * Let the user to select the folder and images inside that folder
+	 */
 	public void selectAndInvert(){
+		
+		//select the folder with images
 		DirectoryResource dr = new DirectoryResource();
 		
+		//for loop iterate all selected files/images
 		for(File inFile : dr.selectedFiles()){
 			
+			//turn the selected file into image
 			ImageResource inImage = new ImageResource(inFile);
 			
+			//pick the original name add "inver-" in front, which is the new name
 			String oriName = inImage.getFileName();
 			String newName = "Inver-" + oriName;
 			
+			//call Class_03_ImageInversion class to create an object imageInvert
+			//use the method imageInvert.invertPixels() to transfer the image color into invert color
 			Class_03_ImageInversion imageInvert = new Class_03_ImageInversion();
 			ImageResource outImage = imageInvert.invertPixels(inImage);
 			
+			//set new name, draw the new image, and save
 			outImage.setFileName(newName);
 			outImage.draw();
+			
 			outImage.save();
+			//outImage.saveAs();
+			
 		}//end for loop;
 		
 	}//end selectAndInvert() method;
