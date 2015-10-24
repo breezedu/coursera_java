c886ac9b78393f329aadc0d44288e21d2d3de570
breezedu <breeze.du@gmail.com>
2015-10-24 14:50:26 -0400

    update FindAGene.java class

--- Week2_FindAGene.java
+++ Week2_FindAGene.java
@@ -2,6 +2,17 @@ package coursera_java_duke;
 
 public class Week2_FindAGene {
 	
+	/********
+	 * Find a gene in a dna string;
+	 * 1st, get the start codon
+	 * 2nd, figure if the next TAG is 3x base away from the start codon, if yes, then get a gene
+	 * 3rd, if TAG is not the ideal stop codon, try TGA
+	 * 4th, if TGA does not work, try TAA
+	 * 5th, return the gene sequence;
+	 * 
+	 * @param inString
+	 * @return
+	 */
 	public String findGene(String inString){
 		
 		//Transfer lower letters into upper letters
@@ -80,15 +91,49 @@ public class Week2_FindAGene {
 	}//end low2Up() method; 
 	
 	
+	/*******
+	 * find the stop codon of a gene
+	 * 1st, if it is an empty string, return "";
+	 * else, return the last three letters of that gene;
+	 * @param gene
+	 * @return
+	 */
+	public String findStopCodon(String gene){
+			
+		if(gene.length() == 0) return "";		
+		else return gene.substring(gene.length() - 3);
+		
+		//return stopCodon;
+		
+	}//end findStopCodon() method;
+	
+	
+	/********
+	 * the main() method;
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args){
 		
+		//create a new FindAGene object;
 		Week2_FindAGene FAG = new Week2_FindAGene();
-		String dna_string = "AATGCTAGTttAAATCTGA";
 		
+		
+		//init the original DNA string;
+		String dna_string = "AATGCTAGTttAAATCTGAGGCCTT";
+		
+		//call FAG.findGene() method to locate a gene inside that DNA string;
 		String gene = FAG.findGene(dna_string);
 		
+		//after locating the gene, call FAG.findStopCodon() method to ge the stop codon of that gene
+		String stopCodon = FAG.findStopCodon(gene);
+		
+		
+		//printout the gene and the stop codon;
 		if(gene.length() == 0) System.out.println("There's no gene found in the dna string.");
 		else System.out.println("The gene: " + gene);
+		
+		System.out.println("The stop codon: " + stopCodon);
 		
 	}//end main();
 	
