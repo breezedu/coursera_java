52b85f4ded060f035b191e85c84b3e0fe1a17efa
breezedu <breeze.du@gmail.com>
2015-10-24 11:19:26 -0400

    add TagFinder.java class

---
+++ Week2_TagFinder.java
@@ -0,0 +1,54 @@
+package coursera_java_duke;
+
+/**
+ * Finds a protein within a strand of DNA represented as a string of c,g,t,a letters.
+ * A protein is a part of the DNA strand marked by start and stop codons (DNA triples)
+ * that is a multiple of 3 letters long.
+ *
+ * @author Duke Software Team 
+ */
+import edu.duke.*;
+import java.io.*;
+
+public class Week2_TagFinder {
+	
+	public String findProtein(String dna) {
+		int start = dna.indexOf("atg");
+		if (start == -1) {
+			return "";
+		}
+		int stop = dna.indexOf("tag", start+3);
+		if ((stop - start) % 3 == 0) {
+			return dna.substring(start, stop+3);
+		}
+		else {
+			return "";
+		}
+	}//end findProtein() method
+	
+	public void testing() {
+		String a = "cccatggggtttaaataataataggagagagagagagagttt";
+		String ap = "atggggtttaaataataatag";
+		//String a = "atgcctag";
+		//String ap = "";
+		//String a = "ATGCCCTAG";
+		//String ap = "ATGCCCTAG";
+		String result = findProtein(a);
+		if (ap.equals(result)) {
+			System.out.println("success for " + ap + " length " + ap.length());
+		}
+		else {
+			System.out.println("mistake for input: " + a);
+			System.out.println("got: " + result);
+			System.out.println("not: " + ap);
+		}
+	}//end testing()
+	
+	
+	public static void main(String[] args){
+		
+		Week2_TagFinder TF = new Week2_TagFinder();
+		TF.testing();
+	}
+	
+}
