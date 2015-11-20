cde5380b61be5e48e0654d0a3dab054524e31894
breezedu <breeze.du@gmail.com>
2015-11-19 22:31:57 -0500

    update Week04 qizz solutions Q10 incorrect

--- Week04_miniProject.java
+++ Week04_miniProject.java
@@ -1,4 +1,4 @@
-package week04;
+package coursera_java_duke;
 
 import java.io.File;
 
@@ -29,22 +29,36 @@ public class Week04_miniProject {
 		
 		
 		//1st Modify the method totalBirths, print out number of girls/boys and total
-		System.out.println("\n Question 1:" );
+		System.out.println("\n Problem 1: printout number of girls/boys, and total" );
+		
+		System.out.println("Q#1: Select year 1900: ");
 		miniP.totalBirths();
 		
-		
+		System.out.println("Q#2: Select year 1905: ");
+		miniP.totalBirths();		
 		
 		//2nd method named getRank that has three parameters: 
 		//an integer named year, a string named name, and a string named gender 
 		//(F for female and M for male). 
 		//This method returns the rank of the name in the file for the given gender
-		System.out.println("\n Question 2:" );
+		System.out.println("\n Problem 2: get rank by name");
 		
-		int year = 2012;
-		String name = "Isabella";
+
+		int year = 1960;
+		String name = "Emily";
 		String gender = "F";
+		System.out.println("Q#3: Select year " + year +". ");
 		int rank = miniP.getRank(year, name, gender);
-		System.out.println("The name " + name + " ranks " + rank + " at year " + year);
+		System.out.println("The name " + name + " gender " + gender + " ranks " + rank + " at year " + year);
+		
+		
+		year = 1971;
+		name = "Frank";
+		gender = "M";
+		System.out.println("Q#4: Select year " + year +". ");
+		rank = miniP.getRank(year, name, gender);
+		System.out.println("The name " + name + " gender " + gender + " ranks " + rank + " at year " + year);
+		
 		
 		
 		
@@ -52,14 +66,22 @@ public class Week04_miniProject {
 		//an integer named year, an integer named rank, and a string named gender 
 		//(F for female and M for male). 
 		//This method returns the name of the person in the file at this rank, for the given gender.
-		System.out.println("\n question 3: ");
+		System.out.println("\n Problem 3: getName by rank");
 		
-		year = 2012;
-		rank = 2;
-		gender = "M";
+		year = 1980;
+		rank = 350;
+		gender = "F";
+		System.out.println("Q#5: Select year " + year +". ");
 		String rankName = miniP.getName(year, rank, gender);
 		System.out.println("The " + gender + " name ranked " + rank + " is: " + rankName +". " );
 		
+		
+		year = 1982;
+		rank = 450;
+		gender = "M";
+		System.out.println("Q#6: Select year " + year +". ");
+		rankName = miniP.getName(year, rank, gender);
+		System.out.println("The " + gender + " name ranked " + rank + " is: " + rankName +". " );
 		
 		
 		//4th What would your name be if you were born in a different year? 
@@ -68,12 +90,21 @@ public class Week04_miniProject {
 		//an integer named newYear and a string named gender (F for female and M for male).
 		//This method determines what name would have been named if they were born in a different year, 
 		//based on the same popularity.
-		System.out.println("\n question 4:" );
+		System.out.println("\n Problem 4: whatIsNameInYear" );
 		
-		int year1 = 2012;
+		int year1 = 1972;
 		int year2 = 2014;
 		gender = "F";
-		name = "Isabella";
+		name = "Susan";
+		System.out.println("Q#7: Select year " + year1 +"; And year " + year2 +". ");
+		miniP.whatIsNameInYear(name, year1, year2, gender);
+		
+		
+		year1 = 1974;
+		year2 = 2014;
+		gender = "M";
+		name = "Owen";
+		System.out.println("Q#8: Select year " + year1 +"; And year " + year2 +". ");
 		miniP.whatIsNameInYear(name, year1, year2, gender);
 		
 		
@@ -82,15 +113,102 @@ public class Week04_miniProject {
 		//a string name, and a string named gender (F for female and M for male). 
 		//This method selects a range of files to process and returns an integer, 
 		//the year with the highest rank for the name and gender.
-		System.out.println("\n question 5: ");
-		name = "Mason"; 
-		gender = "M";
+		System.out.println("\n Problem 5: yearOfHighestRank");
+		
+		name = "Genevieve"; 
+		gender = "F";
+		System.out.println("Q#9: select all years:");
 		int highestRank = miniP.yearOfHighestRank(name, gender);
 		System.out.println("The highest rank for the name " + name + " gender " + gender + " is at year " + highestRank );
 		
 		
 		
+		name = "Mich"; 
+		gender = "M";
+		System.out.println("Q10: select all years:");
+		highestRank = miniP.yearOfHighestRank(name, gender);
+		System.out.println("The highest rank for the name " + name + " gender " + gender + " is at year " + highestRank );
+		
+		
+		
+		//6th method getAverageRank that has two parameters: a string name, and a string named gender (F for female and M for male). 
+		//This method selects a range of files to process and returns a double representing the average rank 
+		//of the name and gender over the selected files.
+		System.out.println("\n question 6: averageRank");
+				
+		name = "Susan";
+		gender = "F";
+		System.out.println("Q#11: select all years:");
+		double aveRank = miniP.getAverageRank(name, gender);
+		System.out.println("The average rank of " + name + " gender " + gender + " is " + aveRank);
+		
+		
+		name = "Robert";
+		gender = "M";
+		System.out.println("Q#11: select all years:");
+		aveRank = miniP.getAverageRank(name, gender);
+		System.out.println("The average rank of " + name + " gender " + gender + " is " + aveRank);
+		
+		
 	}//end of main();
+	
+	
+	
+	
+	
+
+	/******************
+	 * Question 6:
+	 * // For example calling getAverageRank with name Mason and gender ‘M’ 
+	 * and selecting the three test files above results in returning 3.0, 
+	 * as he is rank 2 in the year 2012, rank 4 in 2013 and rank 3 in 2014. 
+	 * @param name
+	 * @param gender
+	 * @return
+	 */
+	private double getAverageRank(String name, String gender) {
+		// TODO Auto-generated method stub
+		
+		//get director
+		DirectoryResource dr = new DirectoryResource();
+		int fileNum = 0;
+		int totalRank = 0;
+		
+		for(File fi : dr.selectedFiles()){
+			fileNum++;
+			
+			//get the file resource
+			FileResource fr = new FileResource(fi);
+			
+			int pivot = 0;
+			int currRank = 0;
+			for(CSVRecord record : fr.getCSVParser(false) ){
+				
+				if(record.get(1).equals(gender)) {pivot++;
+				
+					if(record.get(0).equals(name)) {
+					
+						currRank = pivot;
+						break;
+					
+					} //end if record.equals name condition;
+				}
+				
+			}//end for Record loop;
+			
+			totalRank += currRank;
+			
+		}//end for file loop;
+		
+		if(totalRank == 0) return -1;
+		else return (double)(totalRank)/fileNum;
+	
+	}//end getAverageRank() method;
+
+
+
+
+
 
 	/**********
 	 * 5th question:
@@ -123,13 +241,17 @@ public class Week04_miniProject {
 			int currRank = 0;
 			for(CSVRecord record : fr.getCSVParser(false)){
 				
-				if(record.get(1).equals(gender)) currRank++;
-				
-				if(record.get(0).equals(name)) break;
+				if(record.get(1).equals(gender)) {
+					
+					currRank++;
+								
+					if(record.get(0).equals(name)) break;
+					
+				}
 			}//end for loop;
 			
 			//int currRank = getRank(year, name, gender);
-			System.out.println("  At year " + year + " name " + name + " ranks " + currRank + ". ");
+			System.out.println("  At year " + year + " name " + name + " gender " + gender + " ranks " + currRank + ". ");
 			
 			if(currRank != -1 && currRank < rank){
 				rank = currRank;
@@ -182,13 +304,15 @@ public class Week04_miniProject {
 		//get the CSV file
 		for(CSVRecord record : fr.getCSVParser(false)){
 			
-			if(record.get(1).equals(gender)) pivot ++;
-			
-			if(pivot == rank) return record.get(0);
+			if(record.get(1).equals(gender)){ 
+				
+				pivot ++;
+				if(pivot == rank) return record.get(0);
+			}//
 			
 		}//end for CSV record loop;
 		
-		
+		System.out.println("the rank: " + rank + "... The last one rank " + pivot + ".");
 		return "NO NMAE";
 		
 	}//end getName() method;
@@ -209,26 +333,18 @@ public class Week04_miniProject {
 		//get the file
 		FileResource fr = new FileResource();
 				
-		int pivotF = 0;
-		int pivotM = 0;
+		int pivot = 0;
 		
 		for(CSVRecord record : fr.getCSVParser(false)){
 			
 			//for female records
-			if(record.get(1).equals("F")) {
+			if(record.get(1).equals(gender)) {
 				
-				//the pivot of Female ++
-				pivotF++;
-				if(record.get(0).equals(name)) return pivotF;
-			}
+				//the pivot of equal gender ++
+				pivot++;
+				if(record.get(0).equals(name)) return pivot;
 			
-			//for male reocrds
-			if(record.get(1).equals("M")) {
-				
-				//the pivot of Male ++
-				pivotM++;
-				if(record.get(0).equals(name)) return pivotM;
-			} 
+			}
 			
 		}//end for CSV record loop;
 			
@@ -256,10 +372,10 @@ public class Week04_miniProject {
 			String gender = record.get(1);
 			int num = Integer.parseInt(record.get(2));
 			
-			if(gender.equals("F")) numGirl += num;
-			else numBoy += num;
+			if(gender.equals("F")) numGirl ++;
+			else numBoy ++;
 			
-			sum += num;
+			sum ++;
 			
 		}//end for csvrecord loop;
 		
